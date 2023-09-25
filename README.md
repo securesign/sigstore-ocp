8fbcc40275d8269a50f52310b4f8f9f6b27b63bd
Sally O'Malley <somalley@redhat.com>
2023-09-25 11:21:54 -0400

    add kind
    Signed-off-by: Sally O'Malley <somalley@redhat.com>

--- README.md
+++ README.md
@@ -13,4 +13,31 @@ Information on how to install Sigstore components on OpenShift can be found in t
 
 ## Scaffolding Chart
 
+<<<<<<< HEAD
 More information can be found by inspecting the [trusted-artifact-signer chart](charts/trusted-artifact-signer).
+=======
+More information can be found by inspecting the [scaffolding chart](charts/scaffolding).
+
+## Contributing
+
+Install the [pre-commit](https://pre-commit.com/) package and run `pre-commit run --all-files` before pushing changes, or `pre-commit install` to automatically run the pre-commit hooks with every `git commit`. If it fails,
+run the `git commit` command again. It's likely the pre-commit hook fixed the issue and you have to bring in the new changes.
+
+### Testing
+
+To set up a `kind` cluster and deploy the charts, run the following from the root of this repository
+
+```bash
+./kind/kind-up-test.sh
+
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=90s
+
+OPENSHIFT_APPS_SUBDOMAIN=localhost envsubst <  ./examples/values-kind-sigstore.yaml | helm upgrade -i scaffolding --debug ./charts/scaffolding -n sigstore --create-namespace --values -
+```
+
+This test setup is to verify that all deployments are healthy and all jobs complete. However, this does not create a working environment to sign artifacts.
+
+>>>>>>> a0f7581 (add kind)
---
+++ kind/kind-up-test.sh
@@ -0,0 +1,46 @@
+# run this from root of repository
+
+# spin up kind cluster
+cat <<EOF | sudo kind create cluster --image kindest/node:v1.28.0 --config=-
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+nodes:
+- role: control-plane
+  kubeadmConfigPatches:
+  - |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+EOF
+
+sudo kind get kubeconfig > /tmp/config
+sudo chown $USER:$USER /tmp/config
+mv /tmp/config ~/.kube/config
+
+# install ingress-nginx
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
+#kubectl wait --namespace ingress-nginx \
+#  --for=condition=ready pod \
+#  --selector=app.kubernetes.io/component=controller \
+#  --timeout=90s
+#
+# TODO: add a wait for ingress to be ready with test.yaml & curl
+#sleep 20
+
+oc create ns fulcio-system
+oc create ns rekor-system
+oc -n fulcio-system create secret generic fulcio-secret-rh --from-file=private=./kind/test-keys-cert/file_ca_key.pem --from-file=public=./kind/test-keys-cert/file_ca_pub.pem --from-file=cert=./kind/test-keys-cert/fulcio-root.pem  --from-literal=password=secure --dry-run=client -o yaml | oc apply -f-
+
+oc -n rekor-system create secret generic rekor-private-key --from-file=private=./kind/test-keys-cert/rekor_key.pem --dry-run=client -o yaml | oc apply -f-
+
+# install charts
+#OPENSHIFT_APPS_SUBDOMAIN=localhost envsubst <  ./examples/values-kind-sigstore.yaml | helm upgrade -i scaffolding --debug ./charts/scaffolding -n sigstore --create-namespace --values -
