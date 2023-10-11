b9c48b00a3cc7f474211ddb5855835dc3a80c90b
Jan Bouska <jbouska@redhat.com>
2023-10-11 09:14:48 +0200

    Add sign-verify test

--- kind/kind-up-test.sh
+++ kind/kind-up-test.sh
@@ -1,17 +1,11 @@
 # run this from root of repository
 
 # spin up kind cluster
-cat <<EOF | sudo kind create cluster --image kindest/node:v1.28.0 --config=-
+cat <<EOF | kind create cluster --image kindest/node:v1.28.0 --config=-
 kind: Cluster
 apiVersion: kind.x-k8s.io/v1alpha4
 nodes:
 - role: control-plane
-  kubeadmConfigPatches:
-  - |
-    kind: InitConfiguration
-    nodeRegistration:
-      kubeletExtraArgs:
-        node-labels: "ingress-ready=true"
   extraMounts:
   - containerPath: /var/lib/kubelet/config.json
     hostPath: "${HOME}/.docker/config.json"
@@ -24,8 +18,8 @@ nodes:
     protocol: TCP
 EOF
 
-sudo kind get kubeconfig > /tmp/config
-sudo chown $USER:$USER /tmp/config
+kind get kubeconfig > /tmp/config
+chown $USER:$USER /tmp/config
 if [[ -d ~/.kube ]] && [[ -f ~/.kube/config ]]
 then
   export KUBECONFIG=~/.kube/config:/tmp/config
@@ -37,19 +31,8 @@ fi
 
 oc config use-context kind-kind
 
-# install ingress-nginx
-oc apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-
 # add servicemonitor crd 
 oc apply -f ./kind/servicemonitor-crd.yaml
-
-#oc wait --namespace ingress-nginx \
-#  --for=condition=ready pod \
-#  --selector=app.kubernetes.io/component=controller \
-#  --timeout=90s
-#
-# TODO: add a wait for ingress to be ready with test.yaml & curl
-#sleep 20
 
 oc create ns fulcio-system
 oc create ns rekor-system
@@ -57,5 +40,27 @@ oc -n fulcio-system create secret generic fulcio-secret-rh --from-file=private=.
 
 oc -n rekor-system create secret generic rekor-private-key --from-file=private=./kind/testing-only-cert-key/rekor_key.pem --dry-run=client -o yaml | oc apply -f-
 
+#install OLM
+kubectl create -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.25.0/crds.yaml
+# wait for a while to be sure CRDs are installed
+sleep 1
+kubectl create -f https://github.com/operator-framework/operator-lifecycle-manager/releases/download/v0.25.0/olm.yaml
+
+#install keycloak from Kind overlay
+kubectl create --kustomize keycloak/operator/overlay/kind
+until [ ! -z "$(kubectl get pod -l name=keycloak-operator -n keycloak-system 2>/dev/null)" ]
+do
+  echo "Waiting for keycloak operator. Pods in keycloak-system namespace:"
+  kubectl get pods -n keycloak-system
+  sleep 10
+done
+kubectl create --kustomize keycloak/resources/overlay/kind
+until [[ $( oc get keycloak keycloak -o jsonpath='{.status.ready}' -n keycloak-system 2>/dev/null) == "true" ]]
+do
+  printf "Waiting for keycloak deployment. \n Keycloak ready: %s \n" $(oc get keycloak keycloak -o jsonpath='{.status.ready}' -n keycloak-system)
+  sleep 10
+done
+
 # install charts
-#OPENSHIFT_APPS_SUBDOMAIN=localhost envsubst <  ./examples/values-kind-sigstore.yaml | helm upgrade -i trusted-artifact-signer --debug ./charts/trusted-artifact-signer --wait --wait-for-jobs -n sigstore --create-namespace --values -
+helm upgrade -i trusted-artifact-signer --debug ./charts/trusted-artifact-signer --wait --wait-for-jobs --timeout 10m -n sigstore --create-namespace --values ./examples/values-kind-sigstore.yaml && \
+helm test trusted-artifact-signer -n sigstore
