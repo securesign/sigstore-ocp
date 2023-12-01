2524f995d7a8430ffd01124f3c055f3e413f8a80
Jason Power <20076537@mail.wit.ie>
2023-12-01 15:40:37 +0000

    SECURESIGN-276 | Install helm charts with a binary CLI (#94)
    * enable keycloak install with binary
    * enable cert setup
    * configure pull secret
    * update pull secret logic, add rekor/fulcio secret
    * add helm installation
    * handle backup segment job

---
+++ tas-installer/cmd/install.go
@@ -0,0 +1,95 @@
+package cmd
+
+import (
+	"fmt"
+	"log"
+	"securesign/sigstore-ocp/tas-installer/internal/install"
+	"securesign/sigstore-ocp/tas-installer/pkg/certs"
+	"securesign/sigstore-ocp/tas-installer/pkg/secrets"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	helmChartVersion string
+	helmValuesFile   string
+)
+
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Installs Trusted Artifact Signer",
+	Long: `Installs Trusted Artifact Signer (TAS) on a Kubernetes cluster.
+
+	This command performs a series of actions:
+	1. Initializes the Kubernetes client to interact with your cluster
+	2. Installs Keycloak for SSO
+	3. Sets up necessary certificates
+	4. Configures secrets
+	5. Deploys TAS to openshift`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := installTas(); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(installCmd)
+}
+
+func installTas() error {
+	installSteps := []func() error{
+		func() error {
+			return install.HandleKeycloakInstall(kc, "keycloak/operator/base", "keycloak/resources/base")
+		},
+		func() error { return install.HandleCertSetup(kc) },
+		func() error {
+			return install.DeleteSegmentBackupJobIfExists(kc, "sigstore-monitoring", "segment-backup-job")
+		},
+		func() error { return install.HandleNamespaceCreate(kc, "sigstore-monitoring") },
+		func() error { return install.HandlePullSecretSetup(kc, "pull-secret", "sigstore-monitoring") },
+		func() error { return install.HandleNamespaceCreate(kc, "fulcio-system") },
+		func() error {
+			return secrets.ConfigureSystemSecrets(kc, "fulcio-system", "fulcio-secret-rh", getFulcioLiteralSecrets(), getFulcioFileSecrets())
+		},
+		func() error { return install.HandleNamespaceCreate(kc, "rekor-system") },
+		func() error {
+			return secrets.ConfigureSystemSecrets(kc, "rekor-system", "rekor-private-key", nil, getRekorSecrets())
+		},
+		func() error {
+			return install.HandleHelmChartInstall(kc, helmValuesFile, helmChartVersion)
+		},
+	}
+	for _, step := range installSteps {
+		if err := step(); err != nil {
+			return fmt.Errorf("install step failed: %v", err)
+		}
+	}
+	return nil
+}
+
+func init() {
+	installCmd.PersistentFlags().StringVar(&helmChartVersion, "chartVersion", "0.1.24", "Version of the Helm chart")
+	installCmd.PersistentFlags().StringVar(&helmValuesFile, "valuesFile", "examples/values-sigstore-openshift.yaml", "Custom values file for chart configuration")
+}
+
+func getFulcioFileSecrets() map[string]string {
+	return map[string]string{
+		"private": "./keys-cert/file_ca_key.pem",
+		"public":  "./keys-cert/file_ca_pub.pem",
+		"cert":    "./keys-cert/fulcio-root.pem",
+	}
+}
+
+func getFulcioLiteralSecrets() map[string]string {
+	return map[string]string{
+		"password": certs.GetCertPassword(),
+	}
+}
+
+func getRekorSecrets() map[string]string {
+	return map[string]string{
+		"private": "./keys-cert/rekor_key.pem",
+	}
+}
